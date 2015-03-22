9b3c8e865c40b5459e53e11e5e25a972c03faf75
Viral <viral30september@gmail.com>
2015-03-22 20:19:30 +0530

    App added

--- app/src/main/java/com/example/arjun/oc/MyActivity.java
+++ app/src/main/java/com/example/arjun/oc/MyActivity.java
@@ -11,6 +11,7 @@ public class MyActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_my);
     }
 
