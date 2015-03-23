58588e43c0afc6c3a7e112d13337f7cc662cf48f
Viral <viral30september@gmail.com>
2015-03-23 22:41:15 +0530

    Comment added

--- app/src/main/java/com/example/arjun/oc/MyActivity.java
+++ app/src/main/java/com/example/arjun/oc/MyActivity.java
@@ -29,6 +29,7 @@ public class MyActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.my, menu);
+        //edfadsfdaf
         return true;
     }
 
