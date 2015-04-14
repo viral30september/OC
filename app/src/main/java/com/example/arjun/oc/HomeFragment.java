bc61cda9cc33939297afa1421eba64b92a919e00
kushalmodi9 <kushal.modi9@gmail.com>
2015-04-14 18:35:04 +0530

    Parse + UI(Same size card, imageSlider)

--- app/src/main/java/com/example/arjun/oc/HomeFragment.java
+++ app/src/main/java/com/example/arjun/oc/HomeFragment.java
@@ -7,11 +7,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.daimajia.slider.library.Animations.DescriptionAnimation;
 import com.daimajia.slider.library.SliderLayout;
